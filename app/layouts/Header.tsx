a2725796fe2ff920df78ef85a59a35b3ec7b893c
tomas <72792707+tomas-land@users.noreply.github.com>
2023-06-12 00:50:23 +0300

    feat(header):create mobile menu

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -1,10 +1,16 @@
+'use client'
+
+import { useState } from 'react'
 import Link from 'next/link'
+import { Squash as Hamburger } from 'hamburger-react'
 
 import s from '@styles/layouts/header.module.scss'
 import Logo from '../components/UI components/Logo'
 import Button from '../components/UI components/Button'
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <header className={s.header}>
       <div className={s.container}>
@@ -15,27 +21,56 @@ const Header = () => {
           <Link href="">
             <Button label="sign-in" />
           </Link>
+          <div className={s.hamburger_wrapper}>
+            <Hamburger toggled={isOpen} toggle={setOpen} size={25} rounded />
+          </div>
         </div>
         <div className={s.menu}>
           <div className={s.menu_list}>
             <li className={s.menu_item}>
-              <Link href="./" className={s.link}> Home </Link>
+              <Link href="" className={s.link}> Home </Link>
             </li>
             <li className={s.menu_item}>
-              <Link href="" className={s.link}> Movies </Link>
+              <Link href="/movies" className={s.link}> Movies </Link>
             </li>
             <li className={s.menu_item}>
-              <Link href="" className={s.link}> TV Shows </Link>
+              <Link href="/tv-shows" className={s.link}> TV Shows </Link>
             </li>
             <li className={s.menu_item}>
-              <Link href="" className={s.link}> Actors </Link>
+              <Link href="/actors" className={s.link}> Actors </Link>
             </li>
             <li className={s.menu_item}>
-              <Link href="" className={s.link}> More </Link>
+              <Link href="/more" className={s.link}> More </Link>
             </li>
           </div>
         </div>
       </div>
+      {/* Mobile menu */}
+      {isOpen ? (<div className={`${s.mobile_menu} ${s.open}`}>
+        <div className={s.hamburger_wrapper}>
+          <Hamburger toggled={isOpen} toggle={setOpen} size={25} rounded duration={0.9} />
+        </div>
+        <div className={s.mobile_menu_list}>
+          <li className={s.mobile_menu_item}>
+            <Link href="./" className={s.link}> Home </Link>
+          </li>
+          <li className={s.mobile_menu_item}>
+            <Link href="/movies" className={s.link}>Movies </Link>
+          </li>
+          <li className={s.mobile_menu_item}>
+            <Link href="/tv-shows" className={s.link}> TV Shows </Link>
+          </li>
+          <li className={s.mobile_menu_item}>
+            <Link href="/actors" className={s.link}> Actors </Link>
+          </li>
+          <li className={s.mobile_menu_item}>
+            <Link href="/more" className={s.link}> More </Link>
+          </li>
+        </div>
+      </div>)
+        : null
+
+      }
     </header>
 
   )
