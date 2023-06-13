8fd9948514f685164a139d979072d297710f1d38
tomas <72792707+tomas-land@users.noreply.github.com>
2023-06-14 01:11:02 +0300

    feat(hero): create hero content

---
+++ app/components/Hero.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+
+import s from '@styles/components/hero.module.scss'
+
+const Hero = () => {
+  return (
+    <section className={s.hero}>
+
+
+      <Image
+        className={s.hero_image}
+        src="/hero_blur_edges.png"
+        alt="picture of a movie"
+        fill
+      />
+      <div className={s.hero_overlay}>
+        <div className={s.hero_content}>
+          <div className={s.hero_title_wrapper}>
+            <h1 className={s.hero_title}>Discover </h1>
+            <h1 className={s.hero_title}>Collect </h1>
+            <h1 className={s.hero_title}>Recall</h1>
+          </div>
+          {/* <p className={s.hero_subtitle}>MyMovie is a place where you can discover new movies, collect your favorites and recall them whenever you want.</p> */}
+          <p className={s.hero_subtitle}>Make your movie database great again</p>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import '@styles/globals.scss'
 import s from '@styles/pages/page.module.scss'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
     <div className={s.home}>
+      <Hero />
+      
       {/* //Banner */}
       {/* //Search */}
       {/* //Latest movies */}
