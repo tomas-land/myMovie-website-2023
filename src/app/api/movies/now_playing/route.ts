f3c2a310f1756e3d542f01d354b025b900347df1
Tomas Landa <tomaslanda1989@gmail.com>
2023-09-18 22:37:08 +0300

    fix: rename api key

--- src/app/api/movies/now_playing/route.ts
+++ src/app/api/movies/now_playing/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { TMDB_BASE_URL, API_KEY } from '@/config';
+import { TMDB_BASE_URL, TMDB_API_KEY } from '@/config';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const endpoint = searchParams.get('endpoint');
 
   try {
-    const response = await fetch(`${TMDB_BASE_URL}/${endpoint}?api_key=${API_KEY}&language=en-US&page=${page}`, { cache: 'no-cache' });
+    const response = await fetch(`${TMDB_BASE_URL}/${endpoint}?api_key=${TMDB_API_KEY}&language=en-US&page=${page}`, { cache: 'no-cache' });
     const data = await response.json();
     return NextResponse.json({ data: data.results });
   } catch (error) {
