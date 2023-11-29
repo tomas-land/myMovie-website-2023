d3a057fc67cf7ed762102e5da64255ff7d9dfa83
Tomas Landa <tomaslanda1989@gmail.com>
2023-11-30 00:34:19 +0200

    feat: create logo

--- src/app/(pages)/movies/page.tsx
+++ src/app/(pages)/movies/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import s from '@/components/shared/logo/logo.module.scss';
+import Image from 'next/image';
 
 const MoviesPage = () => {
     return (
         <div style={{ height: '100vh', textAlign: 'center' }}>
             <h1 style={{ marginTop: '8rem', color: '#854d6c' }}>🎬 Sorry... still in progress 🎬</h1>
+            <Image className={s.image} src="/6.svg" alt="logo" fill/>
         </div>
     );
 }
--- src/components/shared/logo/Logo.tsx
+++ src/components/shared/logo/Logo.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import s from './logo.module.scss'
+import Image from 'next/image'
 
 const Logo = () => {
   return (
-    <div className={s.logo}>myMovies</div>
+    <div className={s.logo}>
+      <Image className={s.image} src="/logo.svg" alt="logo" fill/>
+      </div>
   )
 }
 
