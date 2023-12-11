8c3daad250629afe889864df4ec26936d5176348
Tomas Landa <tomaslanda1989@gmail.com>
2023-12-11 23:19:31 +0200

    feat (dashboard/favorites) : create list component to display favorite movies

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,6 +4,7 @@ import { TMDB_BASE_URL, TMDB_API_KEY } from '@/lib/config';
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const searchQuery = searchParams.get('query');
+  console.log(searchQuery)
 
   try {
     const response = await fetch(`${TMDB_BASE_URL}/search/multi?api_key=${TMDB_API_KEY}&query=${searchQuery}&include_adult=false&language=en-US&page=1`);
